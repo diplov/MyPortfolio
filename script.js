b3bd77623863c29b0c3286782213b818d0cbc882
diplov <diplovkhatri99@gmail.com>
2020-02-25 15:36:10 +0545

    date/time added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function currentTime() {
     var sec = date.getSeconds();
     var midday = "AM";
     midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
-    // hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
